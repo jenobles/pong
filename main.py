280ebb7c1d3f6d3edf434018568ad249e01275df
jenobles <41806914+jenobles@users.noreply.github.com>
2024-04-30 08:04:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+from paddles import Paddle
+import time
+
+t = Turtle()
+paddle_one = Paddle()
+paddle_one.starting_positions = [(-380, 0), (-380, -20), (-380, -40)]
+paddle_two = Paddle()
+paddle_two.starting_positions = [(380, 0), (380, -20), (380, -40)]
+game_is_on = True
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.bgcolor('black')
+screen.title('Pong')
+screen.tracer(0)
+
+t.goto(0, 300)
+t.setheading(270)
+for _ in range(50):
+    t.forward(10)
+    t.color('white')
+    t.forward(10)
+    t.color('black')
+
+screen.listen()
+screen.onkey(paddle_one.up, 'w')
+screen.onkey(paddle_one.down, 's')
+
+screen.onkey(paddle_two.up, 'Up')
+screen.onkey(paddle_two.down, 'Down')
+
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    paddle_one.move()
+    paddle_two.move()
+
+
+
+screen.exitonclick()
---
+++ paddles.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+class Paddle:
+
+    def __init__(self):
+        self.starting_positions = []
+        self.create_paddle(self.starting_positions)
+        self.paddle = []
+        self.head = self.paddle[0]
+        
+    def create_paddle(self, starting_positions):
+        for x in starting_positions:
+            self.paddle_segment(x)
+           
+    def move(self):
+        for index in range(len(self.paddle)-1, 0, -1):
+            new_x = self.paddle[index - 1].xcor()
+            new_y = self.paddle[index - 1].ycor()
+            self.paddle[index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def paddle_segment(self, x):
+        piece = Turtle(shape = 'square')
+        piece.setheading(90)
+        piece.penup()
+        piece.color('white')
+        piece.goto(x)
+        self.paddle.append(piece)
+
+    def up(self):
+        self.head.setheading(90)
+        
+        
+    def down(self):
+        self.head.setheading(270)
+
+        
+
+
